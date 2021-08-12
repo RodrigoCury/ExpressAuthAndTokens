eb0b027b7e915912c518cab4ede105a2f210bd81
Rodrigo Cury <rodrigocury@ufu.br>
2021-08-12 15:54:55 -0300

    Updated Passwords to Hashed

--- src/usuarios/usuarios-modelo.js
+++ src/usuarios/usuarios-modelo.js
@@ -1,59 +1,71 @@
-const usuariosDao = require('./usuarios-dao');
-const { InvalidArgumentError } = require('../erros');
-const validacoes = require('../validacoes-comuns');
+const usuariosDao = require('./usuarios-dao')
+const { InvalidArgumentError } = require('../erros')
+const validacoes = require('../validacoes-comuns')
+const bcrypt = require('bcrypt')
 
 class Usuario {
   constructor(usuario) {
-    this.id = usuario.id;
-    this.nome = usuario.nome;
-    this.email = usuario.email;
-    this.senha = usuario.senha;
+    this.id = usuario.id
+    this.nome = usuario.nome
+    this.email = usuario.email
+    this.senhaHash = usuario.senhaHash
 
-    this.valida();
+    this.valida()
   }
 
   async adiciona() {
     if (await Usuario.buscaPorEmail(this.email)) {
-      throw new InvalidArgumentError('O usuário já existe!');
+      throw new InvalidArgumentError('O usuário já existe!')
     }
 
-    return usuariosDao.adiciona(this);
+    return usuariosDao.adiciona(this)
   }
 
   valida() {
-    validacoes.campoStringNaoNulo(this.nome, 'nome');
-    validacoes.campoStringNaoNulo(this.email, 'email');
-    validacoes.campoStringNaoNulo(this.senha, 'senha');
-    validacoes.campoTamanhoMinimo(this.senha, 'senha', 8);
-    validacoes.campoTamanhoMaximo(this.senha, 'senha', 64);
+    validacoes.campoStringNaoNulo(this.nome, 'nome')
+    validacoes.campoStringNaoNulo(this.email, 'email')
   }
 
-  
+
   async deleta() {
-    return usuariosDao.deleta(this);
-  }
-  
-  static async buscaPorId(id) {
-    const usuario = await usuariosDao.buscaPorId(id);
-    if (!usuario) {
-      return null;
-    }
-    
-    return new Usuario(usuario);
-  }
-  
-  static async buscaPorEmail(email) {
-    const usuario = await usuariosDao.buscaPorEmail(email);
-    if (!usuario) {
-      return null;
-    }
-    
-    return new Usuario(usuario);
+    return usuariosDao.deleta(this)
   }
 
-  static lista() {
-    return usuariosDao.lista();
+  static async buscaPorId(id) {
+    const usuario = await usuariosDao.buscaPorId(id)
+    if (!usuario) {
+      return null
+    }
+
+    return new Usuario(usuario)
+  }
+
+  static async buscaPorEmail(email) {
+    const usuario = await usuariosDao.buscaPorEmail(email)
+    if (!usuario) {
+      return null
+    }
+
+    return new Usuario(usuario)
+  }
+
+  static async lista() {
+    const usuarios = await usuariosDao.lista()
+
+    return usuarios
+  }
+
+  async adicionaSenha(senha) {
+    validacoes.campoStringNaoNulo(senha, 'senha')
+    validacoes.campoTamanhoMinimo(senha, 'senha', 8)
+    validacoes.campoTamanhoMaximo(senha, 'senha', 64)
+    this.senhaHash = await Usuario.gerarSenhaHash(senha)
+  }
+
+  static gerarSenhaHash(senha) {
+    const custoHash = 10
+    return bcrypt.hash(senha, custoHash)
   }
 }
 
-module.exports = Usuario;
+module.exports = Usuario
